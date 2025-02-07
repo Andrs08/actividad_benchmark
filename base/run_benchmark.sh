1c002e3c75ba5c37d56f83b07b3ac81260981a24
andrs08 <chinchillahuelvas@gmail.com>
2025-02-07 16:40:53 -0500

    Correccion del archivo dockerfil

--- base/run_benchmark.sh
+++ base/run_benchmark.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
-
-# Clona el repositorio con los benchmarks
 git clone https://github.com/Andrs08/actividad_benchmark.git /benchmark
 
-# Entra al directorio clonado
 cd /benchmark/lenguajes
 
-# Crear carpeta de resultados si no existe
 mkdir -p /benchmark/resultados
 
 RESULTS_FILE="/benchmark/resultados/tiempos.csv"
